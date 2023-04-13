626fa1cfbe9c79f7491b0a9e10a633dd9c5bec81
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2023-04-13 14:59:44 +0200

    Add project files.

---
+++ Proffessor/Cstudent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professor
+{
+    internal class Cstudent
+    {
+        public string Name { get; set; }
+        public string RegNo { get; set; }
+        public double YearMark { get; set; }
+        public double LastExamMark { get; set; }
+        public double FinalMark { get; set; }
+
+        public string Grade { get; set; }
+
+        public double CalculateFinal(double yearMark, double lastExamMark)
+        {
+            FinalMark = (yearMark + lastExamMark) / 2;
+            return FinalMark;
+        }
+        //public string CalcGrade()
+        //{
+        //    Grade = null;
+
+            //if (FinalMark > 74)
+            //{ 
+            //    Grade = "A";
+            //    return Grade;
+            //}
+                
+            //else if (FinalMark < 75)
+            //{
+            //    Grade = "B";
+            //    return Grade;
+            //}
+            //else if (FinalMark < 70)
+            //{
+            //    Grade = "C";
+            //    return Grade;
+            //}
+            //else if (FinalMark < 60)
+            //{
+            //    Grade = "D";
+            //    return Grade;
+            //}
+            //else if (FinalMark < 50)
+            //{
+            //    Grade = "F";
+            //    return Grade;
+            //}
+            //return Grade;
+
+
+            //switch (FinalMark)
+            //{
+            //    case < 50:
+            //        Grade = "F";
+            //        break;
+
+            //    case < 60:
+            //        Grade = "D";
+            //        break;
+
+            //    case < 70:
+            //        Grade = "C";
+            //        break;
+
+            //    case > 75:
+            //        Grade = "B";
+            //        break;
+
+            //    case >= 75:
+            //        Grade = "A";
+            //        break;        
+            //}
+
+        //}
+    }
+}
---
+++ Proffessor/Program.cs
@@ -0,0 +1,64 @@
+﻿using Professor;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student = new Cstudent();
+
+        student.Name = "Sphephelo";
+        student.RegNo = "0007";
+        Console.WriteLine("Year Mark: ");
+        student.YearMark = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Last Year Mark: ");
+        student.LastExamMark = Convert.ToDouble(Console.ReadLine());
+        student.FinalMark = 0;
+        /*student.Grade = student.CalcGrade();*/ // this was the issue
+
+        //Console.WriteLine("Name: ");
+        //name = Console.ReadLine();
+        //Console.WriteLine("Registration No.: ");
+        //regNo = Console.ReadLine();
+        //Console.WriteLine("Year Mark: ");
+        //yrMark = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("Last Year Mark: ");
+        //examMark = Convert.ToDouble(Console.ReadLine());
+
+        student.FinalMark = student.CalculateFinal(student.YearMark, student.LastExamMark);
+
+        switch (student.FinalMark)
+        {
+            case < 50:
+                student.Grade = "F";
+                break;
+
+            case < 60:
+                student.Grade = "D";
+                break;
+
+            case < 70:
+                student.Grade = "C";
+                break;
+
+            case < 75:
+                student.Grade = "B";
+                break;
+
+            case >= 75:
+                student.Grade = "A";
+                break;
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Name is " + student.Name);
+        Console.WriteLine("Registration Number is " + student.RegNo);
+        Console.WriteLine("Year Mark is " + student.YearMark);
+        Console.WriteLine("Last Examination Mark is " + student.LastExamMark);
+        Console.WriteLine();
+        Console.WriteLine("Final Mark is " + student.FinalMark);
+        Console.WriteLine("Grade = " + student.Grade);
+    }
+}
+
